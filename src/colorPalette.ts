f3bb7a0f85a22a5cffb96d7559ee6473208ac67d
Tejas M <tejasm@MacBook-Pro-137.local>
2024-11-06 12:57:24 +0530

    added props and themes to the jsontree

---
+++ src/colorPalette.ts
@@ -0,0 +1,12 @@
+export const colorSchemes = {
+    dark: {
+      backgroundColor: "#000",
+      keyTextColor: "#00d084",
+      valueTextColor: "#ff6900",
+    },
+    light: {
+      backgroundColor: "#fff",
+      keyTextColor: "#333",
+      valueTextColor: "#555",
+    },
+  };
