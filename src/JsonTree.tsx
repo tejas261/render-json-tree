a31a909a829020ef87b5d7b3acbdbce8d1cfcfa8
Tejas M <tejasm@MacBook-Pro-137.local>
2024-11-06 12:57:17 +0530

    added props and themes to the jsontree

--- src/JsonTree.tsx
+++ src/JsonTree.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { ChevronDown, ChevronRight, Copy, Plus } from "lucide-react";
+import { colorSchemes } from "./colorPalette";
 
 type JsonData = {
   [key: string]:
@@ -13,10 +14,32 @@ type JsonData = {
 
 interface EditableJsonTreeProps {
   data: JsonData;
-  index: number;
+  width: string;
+  viewStyle?: "dark" | "light";
+  backgroundColor?: string;
+  keyTextColor?: string;
+  valueTextColor?: string;
+  fontSize?: string;
+  fontStyle?: string;
 }
 
-export const JsonTree: React.FC<EditableJsonTreeProps> = ({ data, index }) => {
+export const JsonTree: React.FC<EditableJsonTreeProps> = ({
+  data,
+  width,
+  viewStyle = "light",
+  backgroundColor,
+  keyTextColor,
+  valueTextColor,
+  fontSize,
+  fontStyle,
+}) => {
+  // Apply color scheme based on viewStyle, falling back to specific props if provided
+  const selectedColors = colorSchemes[viewStyle];
+  const appliedBackgroundColor =
+    backgroundColor || selectedColors.backgroundColor;
+  const appliedKeyTextColor = keyTextColor || selectedColors.keyTextColor;
+  const appliedValueTextColor = valueTextColor || selectedColors.valueTextColor;
+
   const [jsonData, setJsonData] = useState<JsonData>(data);
   const [editingKey, setEditingKey] = useState<string | null>(null);
   const [editingValue, setEditingValue] = useState<string>("");
@@ -50,21 +73,6 @@ export const JsonTree: React.FC<EditableJsonTreeProps> = ({ data, index }) => {
     setEditingValue("");
   };
 
-  const updateSessionStorage = (updatedData: JsonData) => {
-    const integrationFlow = JSON.parse(
-      sessionStorage.getItem("integrationFlow") || "{}"
-    );
-
-    if (integrationFlow?.apis) {
-      integrationFlow.apis[index] = { ...updatedData };
-      sessionStorage.setItem(
-        "integrationFlow",
-        JSON.stringify(integrationFlow)
-      );
-      console.log("Flow changes", integrationFlow);
-    }
-  };
-
   const toggleExpand = (key: string) => {
     setExpandedKeys((prevState) => ({
       ...prevState,
@@ -72,8 +80,8 @@ export const JsonTree: React.FC<EditableJsonTreeProps> = ({ data, index }) => {
     }));
   };
 
-  const copyToClipboard = (data: any) => {
-    const contentToCopy = JSON.stringify(data, null, 2);
+  const copyToClipboard = () => {
+    const contentToCopy = JSON.stringify(jsonData, null, 2);
     navigator.clipboard
       .writeText(contentToCopy)
       .then(() => console.log("Content copied to clipboard"))
@@ -88,10 +96,8 @@ export const JsonTree: React.FC<EditableJsonTreeProps> = ({ data, index }) => {
     }
 
     if (Array.isArray(current)) {
-      // If the target is an array, push a new object into the array
       current.push({ key: newKey, value: newValue });
     } else {
-      // If the target is an object, add a new key-value pair
       (current as JsonData)[newKey] = newValue;
     }
 
@@ -113,34 +119,49 @@ export const JsonTree: React.FC<EditableJsonTreeProps> = ({ data, index }) => {
         return (
           <div
             key={fullKey}
-            style={{ marginLeft: "14px", marginBottom: "5px" }}
+            style={{
+              marginLeft: "14px",
+              marginBottom: "5px",
+              backgroundColor: appliedBackgroundColor,
+            }}
             onMouseEnter={() => setHoveredKey(fullKey)}
             onMouseLeave={() => setHoveredKey(null)}
           >
             <div>
-              {keyIndex === 0 && (
-                <Copy
-                  size={28}
-                  className="absolute right-6 border p-1 rounded-sm cursor-pointer top-[4rem]"
-                  onClick={() => copyToClipboard(data)}
-                />
-              )}
               {isObject ? (
                 <>
                   <div
+                    style={{
+                      cursor: "pointer",
+                      display: "flex",
+                      alignItems: "center",
+                    }}
                     onClick={() => toggleExpand(fullKey)}
-                    className="cursor-pointer mt-4 flex items-center"
                   >
                     {isExpanded ? (
                       <ChevronDown color="#B8B8B8" size={16} />
                     ) : (
                       <ChevronRight color="#B8B8B8" size={16} />
                     )}
-                    <h1 className="text-sm font-medium mx-2">{key}</h1>
+                    <h1
+                      style={{
+                        fontSize: fontSize || "1rem",
+                        lineHeight: "1.25rem",
+                        fontWeight: 700,
+                        paddingLeft: "0.5rem",
+                        color: appliedKeyTextColor,
+                      }}
+                    >
+                      {key}
+                    </h1>
                     {hoveredKey === fullKey && (
                       <Plus
+                        style={{
+                          cursor: "pointer",
+                          marginLeft: "4px",
+                        }}
+                        color={`${viewStyle == "dark" ? "#fff" : "#000"}`}
                         size={16}
-                        className="cursor-pointer ml-2"
                         onClick={() => {
                           setEditingField(true);
                           setEditingKey(fullKey);
@@ -150,20 +171,32 @@ export const JsonTree: React.FC<EditableJsonTreeProps> = ({ data, index }) => {
                   </div>
                   {isExpanded && renderTree(data[key] as JsonData, fullKey)}
                   {editingKey === fullKey && editingField && (
-                    <div className={`ml-8 mt-1`}>
+                    <div style={{ paddingLeft: "2rem", marginTop: "4px" }}>
                       <input
                         type="text"
                         placeholder="Key"
                         value={newKey}
                         onChange={(e) => setNewKey(e.target.value)}
-                        className="border-none outline-none p-1 rounded mr-2"
+                        style={{
+                          border: "none",
+                          outline: "none",
+                          padding: "4px",
+                          borderRadius: "4px",
+                          marginRight: "8px",
+                        }}
                       />
                       <input
                         type="text"
                         placeholder="Value"
                         value={newValue}
+                        style={{
+                          border: "none",
+                          outline: "none",
+                          padding: "4px",
+                          borderRadius: "4px",
+                          marginRight: "8px",
+                        }}
                         onChange={(e) => setNewValue(e.target.value)}
-                        className="border p-1 rounded"
                         onKeyDown={(e) => {
                           if (e.key == "Enter") handleAddNewField(fullKey);
                         }}
@@ -173,21 +206,36 @@ export const JsonTree: React.FC<EditableJsonTreeProps> = ({ data, index }) => {
                 </>
               ) : (
                 <div
-                  className="flex ml-5 mt-1"
+                  style={{
+                    display: "flex",
+                    paddingLeft: "1.25rem",
+                    marginTop: "4px",
+                  }}
                   onClick={() => handleEdit(fullKey, String(data[key]))}
                 >
-                  <span className="text-toast-info">{key}: </span>
+                  <span style={{ color: appliedKeyTextColor }}>{key}: </span>
                   {editingKey === fullKey ? (
                     <input
                       type="text"
                       value={editingValue}
                       onChange={handleChange}
-                      className="w-[80%] border-none  outline-none text-success-500 overflow-scroll"
+                      style={{
+                        border: "none",
+                        outline: "none",
+                        overflow: "scroll",
+                      }}
                       onBlur={() => handleBlur(fullKey)}
                       autoFocus
                     />
                   ) : (
-                    <span className="text-success-500 w-[83%] truncate">
+                    <span
+                      style={{
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap",
+                        color: appliedValueTextColor,
+                      }}
+                    >
                       {JSON.stringify(data[key])}
                     </span>
                   )}
@@ -199,5 +247,38 @@ export const JsonTree: React.FC<EditableJsonTreeProps> = ({ data, index }) => {
       });
   };
 
-  return <div>{renderTree(jsonData)}</div>;
+  return (
+    <div
+      className={`${fontStyle || "source-code-pro"}`}
+      style={{
+        padding: "1rem",
+        backgroundColor: appliedBackgroundColor,
+        width: width,
+        margin: "5px",
+        borderRadius: "8px",
+        border: "1px solid grey",
+      }}
+    >
+      {/* Copy icon at the top of the JSON tree */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          marginBottom: "4px",
+        }}
+      >
+        <Copy
+          color={`${viewStyle == "dark" ? "#fff" : "#000"}`}
+          size={20}
+          style={{
+            cursor: "pointer",
+            border: "1px",
+            borderRadius: "2px",
+          }}
+          onClick={copyToClipboard}
+        />
+      </div>
+      {renderTree(jsonData)}
+    </div>
+  );
 };
